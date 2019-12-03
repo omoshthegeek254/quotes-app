0a12da01bc39448178496a722436ffe679bdef19
omoshthegeek <omoshthegeek@gmail.com>
2019-12-03 18:03:28 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-    showDescription: boolean;
-    constructor(public id: number,public quote: string,public description: string,public postDate: Date){
-      this.showDescription=false;
-    }
-  }
